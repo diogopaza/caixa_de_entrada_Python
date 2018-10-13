b215bcc7a83857e4171c5691fb456703a0c3ce49
Diogo Paza <diogopazacvel@gmail.com>
2018-10-13 14:28:12 -0300

    authentication

--- exemplo_dois_dev_media.py
+++ exemplo_dois_dev_media.py
@@ -23,7 +23,40 @@ class Application:
         self.titulo["font"] = ("Arial", "10", "bold")
         self.titulo.pack()
 
+        self.nomeLabel = Label( self.segundoContainer, text="Nome")
+        self.nomeLabel.pack( side=LEFT )
+
+        self.nome = Entry( self.segundoContainer )
+        self.nome["width"]=30
+        self.nome.pack( side=LEFT )
+
+        self.senhaLabel = Label( self.terceiroContainer, text="Senha")
+        self.senhaLabel.pack( side=LEFT )
+
+        self.senha = Entry( self.terceiroContainer )
+        self.senha["width"]=30
+        self.senha["show"]="*"
+        self.senha.pack( side=LEFT )
+
+        self.autenticar = Button( self.quartoContainer )
+        self.autenticar["text"]="Autenticar"
+        self.autenticar["width"]=12
+        self.autenticar["command"]=self.verificaSenha
+        self.autenticar.pack()
+
+        self.mensagem = Label( self.quartoContainer, text=""  )
+        self.mensagem.pack()
+    
         
+    def verificaSenha(self):
+        print("autenticando")
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == "123" and senha=="123":
+            self.mensagem["text"] = "Autenticado"
+        else:
+            self.mensagem["text"] = "Erro na autenticação"
+
 
 janela = Tk()
 Application( janela )
