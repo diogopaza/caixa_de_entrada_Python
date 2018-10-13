c57f3cfc1c1ffc20f120dd5fc01e6653ed9ae0e2
Diogo Paza <diogopazacvel@gmail.com>
2018-10-13 08:44:40 -0300

    buttom

--- dev_media_exemplo.py
+++ dev_media_exemplo.py
@@ -6,6 +6,15 @@ class Application:
         self.widget1.pack()
         self.msg = Label( self.widget1, text="Primeiro widget" )
         self.msg.pack()
+        self.sair = Button(self.widget1)
+        self.sair["text"] = "Sair"
+        self.sair["font"] = ("Calibri","10")
+        self.sair["width"] = 5
+        self.sair["command"] = self.widget1.quit
+        self.sair.pack()
+
+
+        
     
 minha_janela = Tk()
 Application( minha_janela )
