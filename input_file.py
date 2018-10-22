3eee9114844202b2293139ad715be270981e5b82
Diogo Paza <diogopazacvel@gmail.com>
2018-10-21 21:00:34 -0300

    input files

--- input_file.py
+++ input_file.py
@@ -1,14 +1,14 @@
 from tkinter import filedialog
 from tkinter import *
-
+import cv2
 
 root = Tk()
 
 def clicked():
     root.file_name =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
     print(root.file_name)
-
-    
+    imagem = cv2.imread(root.file_name)
+    cv2.imshow("Input", imagem)
 
 
 btn = Button(root, text="Selecionar imagem", command=clicked)
